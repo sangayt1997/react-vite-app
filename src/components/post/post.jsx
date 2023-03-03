8bbd81382fe91534bab8cc8d725691921514972a
Sangay Thinley <thinley.sangay@selise.ch>
2023-03-03 13:59:53 +0600

    outputting dynamic values

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,7 +1,9 @@
+const name = ['Maximilian', 'Manual'];
 function Post() {
+    const chosenName = Math.random() > 0.5 ? name[0] : name[1];
     return (
         <div>
-            <p>Maximilian</p>
+            <p>{chosenName}</p>
             <p>Learning React with vite!</p>
         </div>
     )
