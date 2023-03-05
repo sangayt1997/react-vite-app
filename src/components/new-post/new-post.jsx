2d9cefe1c5f5915f86f0b87765513f7d5205e961
Sangay Thinley <thinley.sangay@selise.ch>
2023-03-05 23:43:30 +0600

    submitting the form in post list

--- src/components/new-post/new-post.jsx
+++ src/components/new-post/new-post.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './new-post.module.css';
 
-function NewPost({onAuthorChange, onTitleChange, onCancel}) {
+function NewPost({onCancel}) {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('')
+
+    function titleChangeHandle(event) {
+        setEnteredTitle(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const postData = {
+            author: enteredAuthor,
+            title: enteredTitle
+        }
+        console.log(postData);
+        onCancel()
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <p>
                 <label htmlFor="name">Your name</label>
-                <input type="text" id="name" required onChange={onAuthorChange}/>
+                <input type="text" id="name" required onChange={authorChangeHandler}/>
             </p>
             <p>
                 <label htmlFor="title">Title</label>
-                <textarea id="title" required rows={3} onChange={onTitleChange}/>
+                <textarea id="title" required rows={3} onChange={titleChangeHandle}/>
             </p>
             <p className={classes.actions}>
                 <button type="button" onClick={onCancel}>Cancel</button>
--- src/components/posts-list/posts-list.jsx
+++ src/components/posts-list/posts-list.jsx
@@ -1,47 +1,19 @@
-import { useState } from "react";
 import classes from "./posts-list.module.css";
 import Post from "../post/post.jsx";
 import NewPost from "../new-post/new-post.jsx";
 import Modal from "../modal/modal.jsx";
 
 function PostsList({isPosting, onStopPosting}) {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('')
-
-    function titleChangeHandle(event) {
-        setEnteredTitle(event.target.value);
-    }
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
-   /* let modalContent;
-
-    if (modalIsVisible) {
-        modalContent = (
-            <Modal onClose={hideModalHandler}>
-                <NewPost
-                    onAuthorChange={authorChangeHandler}
-                    onTitleChange={titleChangeHandle}
-                />
-            </Modal>
-        );
-    }
-*/
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost
-                        onAuthorChange={authorChangeHandler}
-                        onTitleChange={titleChangeHandle}
                         onCancel={onStopPosting}
                     />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} title={enteredTitle}/>
                 <Post author="Manual" title="React is Awesome with Vite!"/>
             </ul>
         </>
