0536107e6bc5d692bbf89e49a7aadd1d3bb46cc9
Sangay Thinley <thinley.sangay@selise.ch>
2023-03-05 23:20:34 +0600

    adding header componnets

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import PostsList from "./components/posts-list/posts-list.jsx";
+import Header from "./components/header/header.jsx";
 
 function App() {
-  return (
-      <main>
-          <PostsList />
-      </main>
-  );
+    const [modalIsVisible, setModalIsVisible] = useState(true);
+
+    function showModalHandler() {
+        setModalIsVisible(true);
+    }
+
+    function hideModalHandler() {
+        setModalIsVisible(false);
+    }
+
+    return (
+        <>
+            <Header onCreatePost={showModalHandler}/>
+            <main>
+                <PostsList isPosting={modalIsVisible} onStopPosting={hideModalHandler}/>
+            </main>
+        </>
+    );
 }
 
 export default App;
